a054b7cb326da4edef5b492ffcccb7f63b223c74
Keith O'Hara <kthohr@users.noreply.github.com>
2018-01-22 11:19:36 -0500

    update tests

--- tests/unconstrained/de.cpp
+++ tests/unconstrained/de.cpp
@@ -1,9 +1,24 @@
-//
-// DE tests
-//
-// g++-mp-7 -O2 -Wall -std=c++11 -I/opt/local/include de.cpp -o de.test -L/opt/local/lib -loptim -framework Accelerate
-// g++-mp-7 -O2 -Wall -std=c++11 -I./../../include de.cpp -o de.test -L./../.. -loptim -framework Accelerate
-//
+/*################################################################################
+  ##
+  ##   Copyright (C) 2016-2018 Keith O'Hara
+  ##
+  ##   This file is part of the OptimLib C++ library.
+  ##
+  ##   OptimLib is free software: you can redistribute it and/or modify
+  ##   it under the terms of the GNU General Public License as published by
+  ##   the Free Software Foundation, either version 2 of the License, or
+  ##   (at your option) any later version.
+  ##
+  ##   OptimLib is distributed in the hope that it will be useful,
+  ##   but WITHOUT ANY WARRANTY; without even the implied warranty of
+  ##   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  ##   GNU General Public License for more details.
+  ##
+  ################################################################################*/
+
+/*
+ * DE tests
+ */
 
 #include "optim.hpp"
 #include "./../test_fns/test_fns.hpp"
